3d0c4689f630cd081b8eeaf8c41c6f2e596a09ef
Spon'k <99675158+Sponk1@users.noreply.github.com>
2023-12-11 21:48:18 +0300

    da

---
+++ caseDate.js
@@ -0,0 +1,10 @@
+var skins = [
+    {name: "pistolet", price: "2500"},
+    {name: "big one", price: "1000"},
+    {name: "bigger maaa", price: "3400"},
+    {name: "desert eagl", price: "6000"}
+];
+
+if(typeof module !== 'undefined' && module.exports){
+    module.exports = skins;
+}
